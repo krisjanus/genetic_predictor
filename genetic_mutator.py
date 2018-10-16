477fba46b053184a0ef962a268f3462f5047eb5f
Christiaan Hattingh <krisjan@simulasie.co.za>
2018-10-16 07:01:38 +0200

    mutation: probability for a particular cube to mutate, and then probability for each component to mutate...checks to make sure new mutated individual is not the same as the original.

--- genetic_mutator.py
+++ genetic_mutator.py
@@ -20,22 +20,29 @@ def gen_split(column_dtype, lower, upper, probability=1, cur_split=np.nan):
     else:
         return cur_split
 
-def mutate(surv_series, df_dtypes, bounds, probability = 1):
+def mutate(surv_series, df_dtypes, bounds, centre_probability = .2, probability = .5):
     mut_series = pd.Series()
     for ind in surv_series.index:
         new_part = pd.DataFrame(index = surv_series[ind].index)
+        not_same_sum = 0
         for col in surv_series[ind]:
             centre = surv_series[ind].loc[:,col]
-            new_centre = {}
-            for index in centre.index:
-                lower = bounds.loc[index,'lower']
-                upper = bounds.loc[index,'upper']
-                col_dtype = df_dtypes[index]
-                new_centre[index] = gen_split(col_dtype, lower, upper, 
-                          probability, centre[index])
-            new_part.loc[:,col] = pd.Series(new_centre)
-        new_ind = ind + '_mut'
-        mut_series[new_ind] = new_part
+            if centre_probability >= random.random():
+                new_centre = {}
+                for index in centre.index:
+                    lower = bounds.loc[index,'lower']
+                    upper = bounds.loc[index,'upper']
+                    col_dtype = df_dtypes[index]
+                    new_centre[index] = gen_split(col_dtype, lower, upper, 
+                              probability, centre[index])
+                new_centre = pd.Series(new_centre)
+            else:
+                new_centre = centre
+            new_part.loc[:,col] = new_centre
+            not_same_sum = not_same_sum + int(sum(new_part.loc[:,col] != centre) > 0)
+        if not_same_sum > 0:
+            new_ind = ind + '_mut'
+            mut_series[new_ind] = new_part
     return mut_series
             
     
--- work_file.py
+++ work_file.py
@@ -31,3 +31,4 @@ pop = gen_part.gen_pop(X_train, 100)
 # evaluate each individual and return an information gain score
 df_scores = part_eval.get_gain_scores(pop, X_train, y_train)
 #%% testing mutation functionality
+surv_mut = gen_mut.mutate(survivors, X_train.dtypes, bounds, centre_probability = .01,probability=.01)
