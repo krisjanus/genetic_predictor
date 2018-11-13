b8c179682fe68c953d514daaa1470313475a5a77
Christiaan Hattingh <krisjan@simulasie.co.za>
2018-11-13 16:56:52 +0200

    save model to hdf functionality

--- gen_part_class.py
+++ gen_part_class.py
@@ -11,16 +11,15 @@ from sklearn.metrics import roc_auc_score
 
 
 class partition_classifier():
-    def __init__(self, df_partition, df_vic = None,
-                 df_proba = None, info_gain = None, auc = None, part_norm=None):
-        self.part = df_partition
+    def __init__(self, df_partition = None, df_vic = None,
+                 df_proba = None, info_gain = None, auc = None, part_norm=2):
+        self.part = df_partition.astype('float')
 #        self.vectors_in_cubes = df_vic
 #        self.probs_in_cube = df_proba
 #        self.info_gain = info_gain
         self.part_norm = part_norm
 #        self.auc = auc
-        
-        
+               
     def colonize(self, X_train, y_train, X_test=None, y_test=None):
         self.X_train = X_train
         self.y_train = y_train
@@ -45,3 +44,13 @@ class partition_classifier():
             for row in df_vic[ind]:
                 df_predict[row] = self.probs_in_cube.loc[ind,'probability']
         return df_predict
+    
+    def save(self, filename='gpt_model.h5'):
+        self.part.to_hdf(filename, key = 'part', mode='w')
+#        self.vectors_in_cubes.to_hdf(filename, key = 'vectors_in_cubes')
+        self.probs_in_cube.to_hdf(filename, key = 'probs_in_cube')
+        scalars = pd.Series([self.info_gain],index=['info_gain'],name='scalars')
+        scalars['auc'] = self.auc
+        scalars['part_norm'] = self.part_norm
+        scalars.astype('float').to_hdf(filename, key='scalars')
+        
